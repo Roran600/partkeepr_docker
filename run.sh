77e3abf3bffbf911b7d83447c44f2ac235b798b4
Roran60 <matusmatusovsky@gmail.com>
2020-12-19 17:05:03 +0100

    /etc/init.d/apache2 start

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the first process
-/usr/sbin/apache2ctl -k start -D
+/etc/init.d/apache2 start -k start -D
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start my_first_process: $status"
